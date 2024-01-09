facf55a71dc69df8da3ad01662ac88be4f87b22f
Kamilo Vasquez <killo.blanco@gmail.com>
2024-01-08 21:51:03 -0500

    🏷️ Add type definitions for D1Database and ENV in env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="astro/client" />
+
+type D1Database = import('@cloudflare/workers-types/experimental').D1Database
+
+type ENV = {
+  SERVER_URL: string;
+  DB: D1Database;
+};
+
+type Runtime = import('@astrojs/cloudflare').DirectoryRuntime<ENV>;
+
+declare namespace App {
+  interface Locals extends Runtime {
+    user: {
+      name: string;
+      surname: string;
+    };
+  }
+}
